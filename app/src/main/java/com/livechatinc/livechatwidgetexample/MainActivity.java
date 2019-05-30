2912b98238b7a19f00c38b691cfde2724c933ea8
Harrie van der Lubbe <harrie@mindd.eu>
2019-05-30 18:59:15 +0700

    remove toolbar

--- app/src/main/java/com/livechatinc/livechatwidgetexample/MainActivity.java
+++ app/src/main/java/com/livechatinc/livechatwidgetexample/MainActivity.java
@@ -19,8 +19,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
     }
 
 
